1b8df5d07e77984015d16ad4598c606dfe19df81
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-01-04 18:03:40 +0100

    third partie cookies removal

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,7 +1,7 @@
 const Video = ({url, className=""}) => {
     return <>
         <iframe 
-        src={`${url}?autoplay=1&loop=1&muted=1&controls=0&quality=auto&badge=0&autopause=0&player_id=0&app_id=58479`} 
+        src={`${url}?autoplay=1&loop=1&muted=1&controls=0&quality=auto&badge=0&autopause=0&player_id=0&app_id=58479&dnt=1`} 
         frameborder="0" 
         className={"pointer-events-none " + className}
         >
